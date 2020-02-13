b01d6dd1a91f22b116c33a1844ab9c1c7afe5699
rafiul41 <rafiul1994@gmail.com>
2020-02-14 00:14:33 +0600

    Modified css for product

--- components/product/Product.vue
+++ components/product/Product.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="product-contents">
     <img :src="productInfo.background" alt="">
-    <div>{{productInfo.name}}</div>
-    <button v-if="!cartProductList[productInfo._id]" @click="addProductToCart(productInfo)">Add to bag</button>
-    <div v-else>
+    <div class="prod-info">
+      <div class="info-1">{{productInfo.name}}</div>
+      <div class="info-2">{{productInfo.weight}}</div>
+      <div class="info-3">{{'Tk.' + productInfo.cost}}</div>
+    </div>
+    <button class="add-to-cart-btn" v-if="!cartProductList[productInfo._id]" @click="addProductToCart(productInfo)">
+      Add to bag
+    </button>
+    <div class="add-more-btn" v-else>
       <span><button @click="decreaseAmount(productInfo)">-</button></span>
-      {{cartProductList[productInfo._id].inBag}} in bag
+      <div class="in-bag-count">{{cartProductList[productInfo._id].inBag}} in bag</div>
       <span><button @click="addProductToCart(productInfo)">+</button></span>
     </div>
   </div>
@@ -27,10 +33,59 @@
 
 <style lang="scss">
   .product-contents {
+    color: #50525a;
+    margin-bottom: 20px;
+
     img {
       margin: 10px;
       height: 150px;
       width: 233px;
+    }
+
+    .prod-info {
+      .info-1 {
+        font-size: larger;
+      }
+
+      .info-2 {
+        font-size: small;
+      }
+
+      .info-3 {
+        font-size: large;
+        font-weight: bold;
+      }
+    }
+
+    .add-to-cart-btn {
+      width: 200px;
+      background: white;
+      color: rgba(255, 0, 0, 0.53);
+      font-weight: bold;
+
+      &:hover {
+        box-shadow: 0 0 10px #c4c4c4;
+      }
+    }
+
+    .add-more-btn {
+      display: flex;
+      justify-content: space-around;
+      span {
+        button {
+          width: 35px;
+          height: 28px;
+        }
+      }
+
+      .in-bag-count {
+        color: white;
+        font-weight: bold;
+        border: 1px solid #b0b0b6;
+        flex-grow: 1;
+        max-width: 130px;
+        background: rgba(255, 0, 0, 0.64);
+      }
     }
   }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <div class="search-text">
-      Please search for any products
+      Please search for any products on the search bar or navigate the category from left.
     </div>
   </div>
 </template>
 
 <style>
   .search-text {
+    font-weight: bold;
+    color: #6c7078;
+    font-size: larger;
     display: flex;
     justify-content: center;
     text-align: center;
